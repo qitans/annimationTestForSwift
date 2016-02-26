725d2a3b4229d07d8e8d73d95a725dbf29cdb1c7
齐坦 <iqitan@gmail.com>
2016-02-26 11:35:07 +0800

    Initial Commit

---
+++ annimationTest20160226/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  annimationTest20160226
+//
+//  Created by qitan on 16/2/26.
+//  Copyright © 2016年 qitan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
